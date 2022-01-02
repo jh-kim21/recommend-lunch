c313d25482bc9dac7bebd0e15552499148b6b626
jh-kim21 <enginhak@naver.com>
2022-01-02 16:40:46 +0900

    데이터 로딩 하는 부분 추가 조금 이상함.

--- src/views/CommentCRUD.js
+++ src/views/CommentCRUD.js
@@ -11,6 +11,7 @@ const db = firestore.collection("lunchDB")
 
 class CommentCRUD extends Component {
     state = {
+        comments:[],
         user:'',
         comment: ''
     }
@@ -28,19 +29,50 @@ class CommentCRUD extends Component {
     }
 
     inputClick = () => {
-        db.add(this.state)
+       let item = {
+            user: this.state.user,
+            comment: this.state.comment,
+        }
+        db.add(item);
     }
+
+    componentDidMount(){
+       
+         let comments = [];    
+         const snapshot = db.onSnapshot((snapshot)=>{
+             snapshot.forEach((item)=>{
+                let id = item.id;
+                let data = item.data();
+                comments.push({
+                    id: id,
+                    user: data.user,
+                    comment: data.comment,
+                });
+             });
+         });
+        
+        this.setState({
+            comments: comments,
+        });
+
+        console.log(this.state.comments);
+
+    }
+
 
     render() {
         return (
             <Container fluid className="main-content-container px-4">
                 <Row>
                     <ListGroup>
-                        <ListGroupItem>Cras justo odio</ListGroupItem>
+                        {this.state.comments.map(commentItem =>(
+                            <ListGroupItem key={commentItem.id}>{commentItem.comment}</ListGroupItem>
+                        ))}
+                        {/* <ListGroupItem>Cras justo odio</ListGroupItem>
                         <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
                         <ListGroupItem>Morbi leo risus</ListGroupItem>
                         <ListGroupItem>Porta ac consectetur ac</ListGroupItem>
-                        <ListGroupItem>Vestibulum at eros</ListGroupItem>
+                        <ListGroupItem>Vestibulum at eros</ListGroupItem> */}
                     </ListGroup>
                 </Row>
                 <Row>
