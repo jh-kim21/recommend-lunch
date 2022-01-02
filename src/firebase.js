5ca52087c622565175ee17ba4437050dd5eb19d1
jh-kim21 <enginhak@naver.com>
2022-01-02 13:32:40 +0900

    Fire Base CRUD 중 Create 테스트 완료

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
-import firebase from "firebase/app";
-import "firebase/firestore";
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
 
 const firebaseConfig = {
     // firebase 설정과 관련된 개인 정보
--- src/views/CommentCRUD.js
+++ src/views/CommentCRUD.js
@@ -1,37 +1,56 @@
 import React, { Component } from "react";
 import {
     Container, Col, Row, Badge,
-    Card, CardHeader, CardBody, Button,
-    FormTextarea,
-  } from "shards-react";
+    Card, CardHeader, CardBody, Button, FormInput,
+    FormTextarea,ListGroup, ListGroupItem
+} from "shards-react";
 
-  import { firestore } from "../firebase";
+import { firestore } from "../firebase";
 
-  
+const db = firestore.collection("lunchDB")
 
-class CommentCRUD extends Component
-{
+class CommentCRUD extends Component {
     state = {
+        user:'',
         comment: ''
     }
 
-    handleChange = (e) => {
+    handleNameChange = (e) => {
         this.setState({
-          comment: e.target.value
+            user: e.target.value
         })
-      }
-
-    inputClick = () =>{
-        
     }
 
-    render()
-    {
-        return(
-            <div>
-                <FormTextarea type="text" name="comment" placeholder="리뷰" value={this.state.comment} onChange={this.handleChange}></FormTextarea>
-                <Button onClick={this.inputClick}>입 력</Button>
-            </div>
+    handleCommentChange = (e) => {
+        this.setState({
+            comment: e.target.value
+        })
+    }
+
+    inputClick = () => {
+        db.add(this.state)
+    }
+
+    render() {
+        return (
+            <Container fluid className="main-content-container px-4">
+                <Row>
+                    <ListGroup>
+                        <ListGroupItem>Cras justo odio</ListGroupItem>
+                        <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+                        <ListGroupItem>Morbi leo risus</ListGroupItem>
+                        <ListGroupItem>Porta ac consectetur ac</ListGroupItem>
+                        <ListGroupItem>Vestibulum at eros</ListGroupItem>
+                    </ListGroup>
+                </Row>
+                <Row>
+                    <FormInput type ="text" name="user" placeholder="사용자" value={this.state.user} onChange={this.handleNameChange}></FormInput>
+                    <FormTextarea type="text" name="comment" placeholder="리뷰" value={this.state.comment} onChange={this.handleCommentChange}></FormTextarea>
+                    <Button onClick={this.inputClick}>입 력</Button>
+                </Row>
+
+            </Container>
+
         );
     }
 }
